c4605c7581b928d590423505a8030fde98c42c91
muzzyay <muzzyay@gmail.com>
2019-03-04 14:10:55 -0700

    more

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,29 +11,26 @@ module.exports = function (app) {
   });
 
   app.get("/signup", function (req, res) {
-    res.send("../public/html/signup");
+    res.render("register");
+    
   });
 
   app.get("/login", function (req, res) {
-    res.send("../public/html/login");
+    // res.sendfile("public/html/login.html");
+    res.render("login");
   });
 
   // Load singleitem page and pass in an item by id
   app.get("/items/:id", function (req, res) {
     db.Item.findOne({ include: [db.User], where: { id: req.params.id } }).then(function (data) {
-      res.render("singleItem", data[0]);
-  // Load example page and pass in an example by id
-  app.get("/posts/:id", function(req, res) {
-    db.Item.findOne({include: [db.User], where: { id: req.params.id } }).then(function(data) {
-      res.render("singleItem", {
-        post: data
-      });
+      res.render("singleItem", data.dataValues);
+      console.log(data.dataValues);
     });
   });
 
   app.get("/users/:UserId", function (req, res) {
-    db.Item.findAll({ include: [db.User], where: req.params }).then(data => res.render("account", {items: data})).catch(err => res.json(err));
-  })
+    db.Item.findAll({ include: [db.User], where: req.params }).then(data => res.render("account", { items: data })).catch(err => res.json(err));
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function (req, res) {
