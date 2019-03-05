df8e1f15f5d294352c4484aeb32eea7741119285
SLawless <slawless05@gmail.com>
2019-03-05 14:23:21 -0700

    version .51

--- public/js/index.js
+++ public/js/index.js
@@ -30,8 +30,8 @@ var API = {
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function () {
+// refreshItems gets new examples from the db and repopulates the list
+var refreshItems = function () {
   API.getItems().then(function (data) {
     var $examples = data.map(function (example) {
       var $a = $("<a>")
@@ -64,42 +64,3 @@ var refreshExamples = function () {
   });
 };
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function (event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveItem(example).then(function () {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-  $itemPrice.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function () {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteItem(idToDelete).then(function () {
-    refreshItems();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
