c123151f2fca018510a2d58b5c8ba900522182ab
Sum Guy <gjserq@gmail.com>
2019-03-05 12:59:46 -0700

    Commit to Pull

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+var passport = require("./config/passport");
+var session = require("exprss-session");
+
 var db = require("./models");
 
 var app = express();
@@ -11,6 +14,12 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//Session
+app.use(session({ secret: "qwikflip", resave: true,
+saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
